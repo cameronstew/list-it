62866b4016a9ee81529bab84cff45d5aab277ac3
Cameron Webster <cameron.webster@gmail.com>
2015-05-26 11:43:20 -0600

    update URL in email to be prod URL

--- app/mailers/list_mailer.rb
+++ app/mailers/list_mailer.rb
@@ -4,7 +4,7 @@ class ListMailer < ApplicationMailer
   def email_list(recipient_name, recipient_email, list_id)
     @recipient = recipient_name
     @recipient_email = recipient_email
-    @url = "http://localhost:3000/shopping/#{list_id}"
+    @url = "http://insta-list.herokuapp.com/shopping/#{list_id}"
     mail(to: @recipient_email, subject: 'You have a new list!')
   end
 end
