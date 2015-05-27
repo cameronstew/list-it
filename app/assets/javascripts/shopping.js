6aea28e82d527e51d89a5aac7f22e9d937bc13c6
Cameron Webster <cameron.webster@gmail.com>
2015-05-26 19:38:32 -0600

    add shopping js file add update item script

---
+++ app/assets/javascripts/shopping.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('.got-it').on('click', function(){
+    var url = $(this).parent().data('item-url');
+    updateItem(url, "purchased");
+  });
+  $('.skip-it').on('click', function(){
+    var url = $(this).parent().data('item-url');
+    updateItem(url, "skipped");
+  });
+});
+
+
+function updateItem(url, status){
+  $.ajax({
+    type: "PATCH",
+    url: url,
+    data: {item: {status: status}},
+  }).error(function(xhr, ajaxOptions, thrownError){
+    console.log("error");
+  });
+}
