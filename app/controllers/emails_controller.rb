8e74b55bebb57243fb5c51e5e48c7c4fcafb8a98
Cameron Webster <cameron.webster@gmail.com>
2015-05-29 14:41:25 -0600

    add sender email field to emails new view, save value to list author column

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,32 +1,31 @@
 class EmailsController < ApplicationController
-  # before_action :set_list
+  before_action :set_list, only: [:new, :create]
 
   def new
-    @list = List.find(params[:list_id])
     @email = Email.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @email = Email.create(email_params)
     @email.list_id = params[:list_id]
     if @email.save
       ListMailer.email_list(@email.recipient_name, @email.recipient_email, @email.list_id).deliver_now
     end
-    @list.author = @email.recipient_name
-    @list.author_email = @email.recipient_email
+    #@list.author = current_user.full_name
+    @list.author_email = @email.sender_email
     redirect_to lists_path
   end
 
   def skipped_items_email
+    @list = List.find(params[:id])
     @items = params[:items]
-    ListMailer.skipped_items_email("Cam Stewbs", "cameron.webster@gmail.com", @items).deliver_now
-    render json: "complete"
+    ListMailer.skipped_items_email(@list.author, @list.author_email, @items).deliver_now
+    render nothing: true
   end
 
   private
   def email_params
-    params.require(:email).permit(:recipient_email, :recipient_name, :message, :list_id)
+    params.require(:email).permit(:recipient_email, :recipient_name, :message, :list_id, :sender_email)
   end
 
   def set_list
