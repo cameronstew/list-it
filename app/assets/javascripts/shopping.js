0053f15bc9155179def688866c48171a840462cc
Cameron Webster <cameron.webster@gmail.com>
2015-06-02 14:13:51 -0600

    refactor javascript

--- app/assets/javascripts/shopping.js
+++ app/assets/javascripts/shopping.js
@@ -1,21 +1,95 @@
 $(function(){
-  $('.got-it').on('click', function(){
-    var url = $(this).parent().data('item-url');
-    //updateItem(url, "purchased");
+  $("#begin-shopping").on('click', function() {
+    shopping.beginShopping();
   });
-  $('.skip-it').on('click', function(){
-    var url = $(this).parent().data('item-url');
-    //updateItem(url, "skipped");
+
+  var count = 0;
+
+  $('.buttons').on('click', '.got-it, .skip-it', function (e) {
+    e.preventDefault();
+    $(this).parent().parent().hide();
+    $(this).parent().parent().siblings().eq(count).fadeIn(500);
+    count++;
+
+    var status = $(this)[0].className;
+    var url = $(this).parent().parent().data('item-url');
+
+    if(status == "got-it"){
+      shopping.updateItem(url, "purchased");
+    }
+    else{
+      shopping.updateItem(url, "skipped");
+    }
+  });
+
+  $('#submit-list').on('click', function(){
+    shopping.submitList();
   });
 });
 
 
-function updateItem(url, status){
-  $.ajax({
-    type: "PATCH",
-    url: url,
-    data: {item: {status: status}},
-  }).error(function(xhr, ajaxOptions, thrownError){
-    console.log("error");
-  });
-}
+var shopping = (function(){
+  var path = window.location.pathname;
+
+  return{
+    beginShopping: beginShopping,
+    updateItem: updateItem,
+    submitList: submitList
+  }
+
+  function beginShopping(){
+    fadeInFadeOut();
+
+  }
+  function fadeInFadeOut(){
+    $('.item').first().fadeIn(2000);
+    $('.item').first().addClass("current");
+    $('#begin-shopping').hide();
+  }
+
+  function updateItem(url, status){
+    $.ajax({
+      type: "PATCH",
+      url: url,
+      data: {item: {status: status}},
+    }).done(function(){
+      updateReviewList();
+    }).error(function(xhr, ajaxOptions, thrownError){
+      console.log("error");
+    });
+  }
+
+  function updateReviewList(){
+    $('.list-review ul').load(path + " .list-review ul");
+  }
+
+  function submitList(){
+    var slug = $('.list-review').data('list-url');
+    var url = slug+'/submit';
+
+    var skippedItems = [];
+
+    $.get(url, function(items){
+      $(items).each(function(){
+        var item = $(this)[0];
+        var itemStatus = item.status;
+        if(itemStatus == "skipped"){
+          skippedItems.push(item);
+        }
+      });
+      sendSkippedItemsEmail(skippedItems)
+    });
+  }
+
+  function sendSkippedItemsEmail(skippedItems){
+    $.ajax({
+      url: path,
+      method: "POST",
+      data: {items: skippedItems}
+    }).done(function(){
+    }).error(function(){
+      console.log("fudge");
+    });
+  }
+
+})();
