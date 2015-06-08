fec7cb93b769b26b4addaf9215c82547138b0f25
Cameron Webster <cameron.webster@gmail.com>
2015-06-08 12:06:16 -0600

    items slide left and right in shopping index

--- app/assets/javascripts/shopping.js
+++ app/assets/javascripts/shopping.js
@@ -3,21 +3,18 @@ $(function(){
     shopping.beginShopping();
   });
 
-  var count = 0;
-
   $('.buttons').on('click', '.got-it, .skip-it', function (e) {
     e.preventDefault();
-    $(this).parent().parent().hide();
-    $(this).parent().parent().siblings().eq(count).fadeIn(500);
-    count++;
 
     var status = $(this)[0].className;
     var url = $(this).parent().parent().data('item-url');
 
     if(status == "got-it"){
+      shopping.slideItem("left");
       shopping.updateItem(url, "purchased");
     }
     else{
+      shopping.slideItem("right");
       shopping.updateItem(url, "skipped");
     }
   });
@@ -34,17 +31,30 @@ var shopping = (function(){
   return{
     beginShopping: beginShopping,
     updateItem: updateItem,
-    submitList: submitList
+    submitList: submitList,
+    slideItem: slideItem
   }
 
   function beginShopping(){
     fadeInFadeOut();
 
   }
+
   function fadeInFadeOut(){
-    $('.item').first().fadeIn(2000);
+    $('.item').first().fadeIn(700);
     $('.item').first().addClass("current");
     $('#begin-shopping').hide();
+  }
+
+  function slideItem(direction){
+    var nextSib = $('.current').next();
+    $('.current').hide('slide', {direction: direction}, 700);
+    setTimeout(function(){
+      $(nextSib).fadeIn(500);
+    }, 700);
+
+    $('.current').removeClass('current');
+    $(nextSib).addClass('current');
   }
 
   function updateItem(url, status){
@@ -88,7 +98,7 @@ var shopping = (function(){
       data: {items: skippedItems}
     }).done(function(){
     }).error(function(){
-      console.log("fudge");
+      console.log("error");
     });
   }
 
